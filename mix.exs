5e775150367efb392b4052559cfd169f78a237fc
Luke Bakken <lbakken@pivotal.io>
2022-12-12 08:03:06 -0800

    1.0.0 release preparation
    https://groups.google.com/g/rabbitmq-users/c/mSJMjyE20Nk

--- mix.exs
+++ mix.exs
@@ -4,21 +4,22 @@
 ##
 ## Copyright (c) 2007-2020 VMware, Inc. or its affiliates.  All rights reserved.
 
-
 defmodule MixTaskArchiveDeps.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mix_task_archive_deps,
-     version: "0.5.0",
-     elixir: "~> 1.9",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "Mix task to create archives for a project dependencies and Elixir itself",
-     package: package(),
-     deps: deps(),
-     name: "MixTaskArchiveDeps",
-     source_url: "https://github.com/rabbitmq/mix_task_archive_deps"]
+    [
+      app: :mix_task_archive_deps,
+      version: "1.0.0",
+      elixir: "~> 1.11",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: "Mix task to create archives for a project dependencies and Elixir itself",
+      package: package(),
+      deps: deps(),
+      name: "MixTaskArchiveDeps",
+      source_url: "https://github.com/rabbitmq/mix_task_archive_deps"
+    ]
   end
 
   # Configuration for the OTP application
@@ -41,11 +42,14 @@ defmodule MixTaskArchiveDeps.Mixfile do
   defp deps do
     [{:ex_doc, "~> 0.16", only: :dev}]
   end
+
   defp package() do
-    [name: :mix_task_archive_deps,
-     files: ["lib", "mix.exs", "README*", "LICENSE*"],
-     maintainers: ["Luke Bakken", "Michael Klishin"],
-     licenses: ["MPL 2.0"],
-     links: %{"GitHub" => "https://github.com/rabbitmq/mix_task_archive_deps"}]
+    [
+      name: :mix_task_archive_deps,
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Luke Bakken", "Michael Klishin"],
+      licenses: ["MPL 2.0"],
+      links: %{"GitHub" => "https://github.com/rabbitmq/mix_task_archive_deps"}
+    ]
   end
 end
