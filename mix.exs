13151d337da283fdb5575409c191a2da29a503ed
Luke Bakken <lbakken@pivotal.io>
2022-12-12 14:47:30 -0800

    More 1.0.0 prep

--- mix.exs
+++ mix.exs
@@ -30,17 +30,8 @@ defmodule MixTaskArchiveDeps.Mixfile do
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.16", only: :dev}]
+    [{:ex_doc, "~> 0.29.1", only: :dev, runtime: false}]
   end
 
   defp package() do
